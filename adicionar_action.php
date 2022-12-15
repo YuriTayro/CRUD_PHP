977fe9a5eb8e4d7ff575a55cf012ea698f277ebd
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2022-12-15 10:38:15 -0300

    First commit.
    Add files.

---
+++ adicionar_action.php
@@ -0,0 +1,30 @@
+<?php
+require 'config.php';
+require 'dao/UsuarioDaoMysql.php';
+
+$usuarioDao = new UsuarioDaoMysql($pdo);
+
+
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+if ($name&&$email){
+
+
+    if($usuarioDao->findByEmail($email)===false){
+        $novoUsuario = new Usuario();
+        $novoUsuario->setNome($name);
+        $novoUsuario->setEmail($email);
+
+        $usuarioDao->add($novoUsuario);
+
+        header("Location: index.php");
+        exit;
+    }else{
+        header("Location: adicionar.php");
+        exit; 
+    }
+} else {
+    header("Location: adicionar.php");
+    exit;
+}
---
+++ excluir.php
@@ -0,0 +1,15 @@
+<?php
+require 'config.php';
+require 'dao/UsuarioDaoMysql.php';
+
+$usuarioDao = new UsuarioDaoMysql($pdo);
+
+$id = filter_input(INPUT_GET, 'id');
+if($id){
+
+    $usuarioDao->delete($id);
+
+}
+
+header("Location: index.php");
+exit;
